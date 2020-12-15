6561ce7c7ee9aae6eb40a83c884d985481ee26b8
Brendan Loomis <brendanloomis@gmail.com>
2020-12-15 12:40:07 -0800

    Add Header Component and move FEATURES to own file

--- src/App.js
+++ src/App.js
@@ -2,49 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import FeatureList from './FeatureList/FeatureList';
 import Cart from './Cart/Cart';
-
-const FEATURES = {
-  Processor: [
-    {
-      name: '17th Generation Intel Core HB (7 Core with donut spare)',
-      cost: 700
-    },
-    {
-      name: 'Professor X AMD Fire Breather with sidewinder technology',
-      cost: 1200
-    }
-  ],
-  "Operating System": [
-    {
-      name: 'Ubuntu Linux 16.04',
-      cost: 200
-    },
-    {
-      name: 'Bodhi Linux',
-      cost: 300
-    }
-  ],
-  "Video Card": [
-    {
-      name: 'Toyota Corolla 1.5v',
-      cost: 1150.98
-    },
-    {
-      name: 'Mind mild breeze 2000',
-      cost: 1345
-    }
-  ],
-  Display: [
-    {
-      name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
-      cost: 1500
-    },
-    {
-      name: '15.3" HGTV (3840 x 2160) Home makeover edition',
-      cost: 1400
-    },
-  ]
-};
+import FEATURES from './FEATURES/FEATURES';
+import Header from './Header/Header';
 
 class App extends Component {
   state = {
@@ -79,9 +38,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-          <h1>ELF Computing | Laptops</h1>
-        </header>
+        <Header />
         <main>
           <FeatureList 
             features={FEATURES} 
